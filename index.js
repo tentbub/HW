dc41a9ba335d8b6163f242c3d3184bf00bcbd032
James Barlow <tentbub@Jamess-MacBook-Pro.local>
2020-10-30 19:49:43 -0600

    thanks for the help

---
+++ index.js
@@ -0,0 +1,6 @@
+button.addEventListener('click', function(){
+    const hiddenContent=document.getElementById('hiddenContent')style.display='block';
+   const button=document.getElementById('button')style.display='none';
+   let counter = document.getElementById("counter").textContent ()
+     console.log (counter)
+   })
